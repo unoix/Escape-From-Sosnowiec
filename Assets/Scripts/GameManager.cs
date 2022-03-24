ddc1a8355542f28fcf7e86efac5295671c019641
unoix <antektrap@gmail.com>
2022-03-24 16:54:16 +0100

    fuck you, movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject MovementButtonController;
+    [SerializeField] Text moveButtonText;
     int currentTurn;
     bool isPlayerTurn;
+    private bool MoveButtonState;
     private void Awake()
     {
         if (instance == null)
@@ -18,5 +23,23 @@ public class GameManager : MonoBehaviour
             Debug.LogWarning("There is more than one GameManager");
         }
     }
-    
+    private void Start()
+    {
+        MovementButtonController.SetActive(false);
+    }
+    public void MoveBtnPressed()
+    {
+       if(MovementButtonController.activeSelf == false)
+       {
+            MovementButtonController.SetActive(true);
+            moveButtonText.text = "Cancel";
+            Debug.Log("press");
+       }
+       else
+       {
+            MovementButtonController.SetActive(false);
+            moveButtonText.text = "move";
+            Debug.Log("Press");
+       }
+    }
 }
